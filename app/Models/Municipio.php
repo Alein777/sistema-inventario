1233cd74961adea90717897d98bc485acf816c89
carlosarmandorivas06 <carlosarmandorivas06@gmail.com>
2026-03-25 20:57:12 -0600

    feat: model Municipio terminado

--- app/Models/Municipio.php
+++ app/Models/Municipio.php
@@ -1,10 +1,11 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
-
 class Municipio extends Model
 {
-    //
+    protected $fillable = ['nombre', 'id_departamento'];
+    public function departamento()
+    {
+        return $this->belongsTo(Departamento::class, 'id_departamento');
+    }
 }
