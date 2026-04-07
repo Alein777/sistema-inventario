02319b1a44075d465b73cf6fad4d510c751d078e
Oswaldo-Lopez-sv <oswaldo.lopez@facela.com>
2026-04-07 09:30:46 -0600

    feat: modulo_controladores_proveedores_movimientos y modelos

--- app/Models/Movimiento.php
+++ app/Models/Movimiento.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movimiento extends Model
 {
-    //
+    protected $fillable = [
+        'id_producto', 'id_user', 'tipo', 
+        'cantidad', 'stock_anterior', 'stock_nuevo', 'motivo'
+    ];
+
+    public function producto()
+    {
+        return $this->belongsTo(Producto::class, 'id_producto');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
 }
--- app/Models/Proveedor.php
+++ app/Models/Proveedor.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Proveedor extends Model
 {
-    //
+    protected $fillable = [
+        'nombre', 'contacto', 'telefono', 
+        'email', 'tipo', 'id_municipio', 'estado'
+    ];
+
+    public function municipio()
+    {
+        return $this->belongsTo(Municipio::class, 'id_municipio');
+    }
+
+    public function productos()
+    {
+        return $this->hasMany(Producto::class, 'id_proveedor');
+    }
 }
